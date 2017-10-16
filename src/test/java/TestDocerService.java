2c46ad7a2671cb77457244ac9ae45f2be08a44cc
Mirco <amirco@gmail.com>
2017-10-16 09:57:26 +0200

    aggiunto test con commento per chiamata curl

---
+++ src/test/java/TestDocerService.java
@@ -0,0 +1,9 @@
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class TestDocerService {
+
+	final static Logger logger = LoggerFactory.getLogger(TestDocerService.class);
+
+	// curl -X POST --header 'Content-Type: application/json' --header 'Accept: application/json' -d '{"externalId": "protocollo_808737", "acl": "{'21':1,'4':2}" }' 'http://localhost:8080/gdadocer/api/docer/documents/setacl'
+}
